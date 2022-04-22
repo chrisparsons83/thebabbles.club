575f51289dcfa3178debea327b6ef8af8985f970
Chris Parsons <chris@chris-parsons.com>
2022-04-21 23:10:09 -0700

    Closes #3.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("@tailwindcss/forms")],
   daisyui: {
     themes: [
       "light",
