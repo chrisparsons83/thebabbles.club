4d600568ab7bdb66d9ed9a1b9c894a646cbb09a1
Chris Parsons <chris@chris-parsons.com>
2022-04-25 23:22:36 -0700

    Start working on the avatar grouping.

--- app/components/LikeButton.tsx
+++ app/components/LikeButton.tsx
@@ -1,8 +1,8 @@
 import { useFetcher } from "@remix-run/react";
 import { useEffect, useState } from "react";
 import { useSocket } from "~/context";
-import { LikeWithUser } from "~/models/like.server";
-import { MessageWithUser } from "~/models/message.server";
+import type { LikeWithUser } from "~/models/like.server";
+import type { MessageWithUser } from "~/models/message.server";
 
 type Props = {
   message: MessageWithUser;
@@ -45,14 +45,36 @@ export default function LikeButton({ message, emoji }: Props) {
           {emoji}
         </button>
       </fetcher.Form>
-      <span>
+      <div className="avatar-group -space-x-6">
         {message &&
           message.likes &&
           message.likes.length > 0 &&
           message.likes.map((like) => {
-            return like?.user.username;
+            if (!like) return null;
+            const { user } = like;
+            if (user.avatar) {
+              <div className="avatar" key={like.id}>
+                <div className="w-12">
+                  <img
+                    src={user.avatar}
+                    alt={user.username}
+                    className="tooltip"
+                    data-tip={user.username}
+                  />
+                </div>
+              </div>;
+            }
+            return (
+              <div className="avatar placeholder" key={like.id}>
+                <div className="w-12 rounded-full bg-neutral-focus text-neutral-content">
+                  <span className="tooltip" data-tip={user.username}>
+                    {user.username.slice(0, 1).toUpperCase()}
+                  </span>
+                </div>
+              </div>
+            );
           })}
-      </span>
+      </div>
     </>
   );
 }
