47645931e500535ae54872fe97963222c9c90581
Chris Parsons <chris@chris-parsons.com>
2022-05-06 23:29:03 -0700

    Using better options for markdown.

--- app/lib/format.ts
+++ app/lib/format.ts
@@ -1,8 +1,7 @@
-import Autolinker from "autolinker";
 import DOMPurify from "dompurify";
 import MarkdownIt from "markdown-it";
 
 export function getFormattedMessageText(text: string) {
-  const md = new MarkdownIt();
-  return Autolinker.link(md.render(DOMPurify.sanitize(text)));
+  const md = new MarkdownIt({ linkify: true, breaks: true });
+  return md.render(DOMPurify.sanitize(text));
 }
