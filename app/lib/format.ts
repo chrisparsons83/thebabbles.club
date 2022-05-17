d295cb7189bb81f1084c89c3da31ce127e67b5e9
Chris Parsons <chris@chris-parsons.com>
2022-05-16 23:13:37 -0700

    Fix #52.

--- app/lib/format.ts
+++ app/lib/format.ts
@@ -1,7 +1,16 @@
 import DOMPurify from "dompurify";
 import MarkdownIt from "markdown-it";
+import mila from "markdown-it-link-attributes";
 
 export function getFormattedMessageText(text: string) {
   const md = new MarkdownIt({ linkify: true, breaks: true });
+
+  md.use(mila, {
+    attrs: {
+      target: "_blank",
+      rel: "noopener noreferrer",
+    },
+  });
+
   return md.render(DOMPurify.sanitize(text));
 }
