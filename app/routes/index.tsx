799ed990a68c0efcb1cc00946883a28a60ac252d
Chris Parsons <chris@chris-parsons.com>
2022-05-21 00:22:40 -0700

    Fix #28.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,6 +5,7 @@ import { requireActiveUser } from "~/session.server";
 import type { Post } from "~/models/post.server";
 import { getPosts } from "~/models/post.server";
 import { Link, useLoaderData } from "@remix-run/react";
+import ReactTimeAgo from "react-time-ago";
 
 type LoaderData = {
   posts: Post[];
@@ -20,11 +21,32 @@ export default function Index() {
   const data = useLoaderData<LoaderData>();
 
   return (
-    <main>
+    <main className="md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-3">
       {data?.posts.map((post: Post) => (
-        <p key={post.id}>
-          <Link to={`posts/${post.id}`}>{post.title}</Link>
-        </p>
+        <div
+          className="not-prose card mb-4 w-full bg-primary shadow-xl md:mb-0"
+          key={post.id}
+        >
+          <div className="card-body">
+            <h2 className="card-title">{post.title}</h2>
+            <p className="card-text">
+              Posted{" "}
+              <ReactTimeAgo
+                date={new Date(post.createdAt)}
+                locale="en-US"
+                timeStyle="round-minute"
+              />
+            </p>
+            <div className="card-actions justify-end">
+              <Link
+                to={`posts/${post.id}`}
+                className="btn btn-secondary no-underline"
+              >
+                Open Post
+              </Link>
+            </div>
+          </div>
+        </div>
       ))}
     </main>
   );
