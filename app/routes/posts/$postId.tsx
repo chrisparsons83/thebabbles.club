804958d87820c0543def2b5ed56c9f378701d1b1
Chris Parsons <chris@chris-parsons.com>
2022-05-23 23:17:15 -0700

    Cleaning up rogue useRef reference.

--- app/routes/posts/$postId.tsx
+++ app/routes/posts/$postId.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
