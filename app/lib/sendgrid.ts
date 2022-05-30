454bfd03048ccc357967a53730e7fd86ef46b150
Chris Parsons <chris@chris-parsons.com>
2022-05-29 23:27:03 -0700

    Remove extraneous code.

--- app/lib/sendgrid.ts
+++ app/lib/sendgrid.ts
@@ -1,16 +1,5 @@
 import * as sg from "@sendgrid/mail";
 
-export type SendGridError = {
-  response?: {
-    body: string;
-  };
-};
-
-function isSendGridError(error: unknown): error is SendGridError {
-  return (error as SendGridError).response !== undefined;
-}
-
 sg.setApiKey(process.env.SENDGRID_API_KEY || "");
 
 export default sg;
-export { isSendGridError };
