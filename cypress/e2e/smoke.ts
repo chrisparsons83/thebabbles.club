eccf076824b4ec8f4fdce8b954c1b2012db9818d
Chris Parsons <chris@chris-parsons.com>
2023-02-27 21:47:40 -0800

    Remove example test.

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -23,25 +23,4 @@ describe("smoke tests", () => {
 
     cy.get("h1").contains("You have registered!");
   });
-
-  it("should allow you to make a note", () => {
-    const testNote = {
-      title: faker.lorem.words(1),
-      body: faker.lorem.sentences(1),
-    };
-    cy.login();
-
-    cy.visit("/notes");
-    cy.findByText("No notes yet");
-
-    cy.findByRole("link", { name: /\+ new note/i }).click();
-
-    cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
-    cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
-    cy.findByRole("button", { name: /save/i }).click();
-
-    cy.findByRole("button", { name: /delete/i }).click();
-
-    cy.findByText("No notes yet");
-  });
 });
